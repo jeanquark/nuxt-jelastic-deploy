93566d4231017527e56d008dc91422936767c1b8
unknown <jm.kleger@gmail.com>
2020-12-18 18:47:09 +0100

    Test static files access on server

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,12 @@
             <h1 class="title">nuxt-jelastic-deploy</h1>
             <div class="links">
                 Current time: {{ new Date() }}<br />
-                Deploy time: 16:57<br /><br />
+                Deploy time: 18:45<br /><br />
 				Image local: <br /><img src="/images/1920x1080.jpg" width="100" /><br /><br />
 				Image server local: <br /><img src="https://loginmycv.jcloud-ver-jpc.ik-server.com/images/1920x1080.jpg" width="100" /><br /><br />
 				Image server nfs: <br /><img src="https://loginmycv.jcloud-ver-jpc.ik-server.com/loginmycv/data/1920x1080.jpg" width="100" /><br /><br />
 				Image server nfs 2: <br /><img src="https://loginmycv.jcloud-ver-jpc.ik-server.com/data/1920x1080.jpg" width="100" /><br /><br />
-				Image server nfs 3: <br /><img src="/loginmycv/data/1920x1080.jpg" width="100" /><br /><br />
+				Image server nfs 3: <br /><img src="/loginmycv/loginmycv/1920x1080.jpg" width="100" /><br /><br />
 				Image server nfs 4: <br /><img src="https://loginmycv.jcloud-ver-jpc.ik-server.com/loginmycv/1920x1080.jpg" width="100" /><br /><br />
 				Image server nfs 5: <br /><img src="/loginmycv/1920x1080.jpg" width="100" /><br /><br />
 				<!-- Image server nfs 6: <br /><br /><br /> -->
