d177ea42a7393e940ef1c381e39420f2cf95e06e
Jean-Marc <jm.kleger@gmail.com>
2020-12-19 17:30:43 +0100

    Test static files access on server

--- pages/index.vue
+++ pages/index.vue
@@ -11,9 +11,9 @@
                 Image server nfs: <br /><img src="/data/1920x1080.jpg" width="100" /><br /><br />
                 Image server nfs 2: <br /><img src="https://loginmycv.jcloud-ver-jpc.ik-server.com/mounted_data_jm/data/1920x1080.jpg" width="100" /><br /><br />
                 Image server nfs 3: <br /><img src="/1920x1080.jpg" width="100" /><br /><br />
-                Image server nfs 4: <br /><img src="/loginmycv/data/1920x1080.jpg" width="100" /><br /><br />
-                Image server nfs 5: <br /><img src="/loginmycv/1920x1080.jpg" width="100" /><br /><br />
-				Image server nfs 6: <br /><img src="https://loginmycv.jcloud-ver-jpc.ik-server.com/1920x1200.jpg" width="100" /><br /><br />
+                Image server nfs 4: <br /><img src="/loginmycv/images/1920x1080.jpg" width="100" /><br /><br />
+                Image server nfs 5: <br /><img src="loginmycv/images/1920x1080.jpg" width="100" /><br /><br />
+				Image server nfs 6: <br /><img src="https://loginmycv.jcloud-ver-jpc.ik-server.com/loginmycv/images/1920x1080.jpg" width="100" /><br /><br />
                 <!-- Image server nfs 6: <br /><br /><br /> -->
             </div>
         </div>
